efefab2a3af7984e7321049724f59fb0f58c72f6
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-18 22:22:45 +0500

    first

---
+++ Chapter 2/app.js
@@ -0,0 +1,31 @@
+// Q1
+let username = ("Ali");
+document.write(username, "<br>");
+// Q2
+let my_name = ("Karim");
+document.write(my_name, "<br>")
+// Q2 assign in veriables
+document.write("veriables assign", "<br>")
+my_name = ("Muhammad Karim")
+document.write("alert box in show veriables", "<br>")
+// Q3
+let meassage = ("Hello World")
+alert(meassage)
+// Q4
+let age = ("15 years old")
+alert(age)
+let study = ("Certified Mobile App Developmeant")
+alert(study)
+// Q5
+let plzz = ("PLZZA \n PLZZ \n PLZ \n PL \n p ")
+alert(plzz)
+// Q6
+let email = ("My emai address is example@example.com")
+alert(email);
+// Q7
+let meassage1 = ("I am traying to learn from the Book A smarter way to learn javascript")
+alert(meassage1)
+// Q8
+document.write("browser show text", "<br>")
+let showmeassagebrowser = ("Yah! I can write Html content through javascript")
+document.write(showmeassagebrowser, "<br>")
