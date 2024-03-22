470783203e50c7d47c1303ff7b3d527c3f4b49ec
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-22 21:54:08 +0500

    New chapter ad chapter789

---
+++ Chapter 7/app.js
@@ -0,0 +1,23 @@
+// Question1
+let totalCost = 1 + 3 * 4;
+console.log(totalCost);
+let totalCost1 = (1 + 3) * 4;
+console.log(totalCost1);
+// Question2
+let totalCost2 = 1 + (3 * 4);
+console.log(totalCost2);
+// Question3
+let totalCost3 = (1 + 3) * 4;
+console.log(totalCost3);
+// Example
+let resultOfComputation = (2 * 4) * 4 + 2;
+console.log(resultOfComputation);
+let resultOfComputation1 = (2 * 4) * (4 + 2)
+console.log(resultOfComputation1);
+// Question4
+let resultOfComputation2 = ((2 * 4) * 4) + 2;
+console.log(resultOfComputation2);
+// Question5
+let resultOfComputation3 = (2 * 4) * (4 + 2);
+console.log(resultOfComputation3);
+
---
+++ Chapter 9/app.js
@@ -0,0 +1,9 @@
+let spec = prompt("Your species?", "human")
+// Question2
+let question = "Your species?";
+let defaultAnswer = "human";
+let spec1 = prompt(question, defaultAnswer)
+// Question2
+let numberOfCats = prompt("How many cats?");
+let tooManyCats = (numberOfCats + 1);
+alert(tooManyCats)
