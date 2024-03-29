eae2d6708a4fd505af343734bfc7ae3819c7baef
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-03-29 21:50:42 +0500

    first

---
+++ Chapter 10/app.js
@@ -0,0 +1,22 @@
+let x = +prompt("Where does the Pope live?");
+let y = ("vatican")
+if (x === y) {
+    alert("Correct!");
+}
+// let c = ("Not allowed ")
+else {
+
+    alert("Not allowed ")
+}
+let correc_tanswer = ("vatican");
+if (x === correc_tanswer) {
+    alert("Correct!")
+
+}
+let x1 = prompt("Where does the Pope live?");
+let correct8Answer = "vatican";
+if (x1 === correct8Answer) {
+    score++;
+    userIQ = "genius";
+    alert("Correct!");
+}
